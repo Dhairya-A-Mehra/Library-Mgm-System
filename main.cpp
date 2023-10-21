f6f48d9a51d65a14bc992f4c8da41541b9ba6f77
Dhairya <118334190+Dhairya-A-Mehra@users.noreply.github.com>
2023-10-21 12:54:46 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,6 +31,34 @@ public:
     }
 };
 
+class BookIssuing {
+public:
+    static void issueBook(vector<book*>& library) {
+        if (library.empty()) {
+            cout << "Library is empty. Cannot issue a book." << endl;
+            return;
+        }
+
+        cout << "Available Books:" << endl;
+        for (int i = 0; i < library.size(); i++) {
+            cout << i + 1 << ". ";
+            library[i]->putdata();
+        }
+
+        int bookIndex;
+        cout << "Enter the index of the book to issue (1-" << library.size() << "): ";
+        cin >> bookIndex;
+
+        if (bookIndex >= 1 && bookIndex <= library.size()) {
+            cout << "You have issued the following book:" << endl;
+            library[bookIndex - 1]->putdata();
+            // You can add more logic here, such as updating the book's status.
+        } else {
+            cout << "Invalid book index. Please try again." << endl;
+        }
+    }
+};
+
 class textbook : public book
 {
 public:
@@ -133,11 +161,8 @@ int main()
     while (true)
     {
         cout << "\nMenu:" << endl;
-        cout << "1. Add Textbook" << endl;
-        cout << "2. Add Novel" << endl;
-        cout << "3. Add Reference Book" << endl;
-        cout << "4. Add Fiction Book" << endl;
-        cout << "5. List Books" << endl;
+        cout << "1. Issue a Book" << endl;
+        cout << "2. Add a Book" << endl;
         cout << "0. Exit" << endl;
         cout << "Enter your choice: ";
         int choice;
@@ -146,45 +171,73 @@ int main()
         switch (choice)
         {
             case 1:
-                library.push_back(new textbook());
-                library.back()->getdata();
+                // Implement book issuing logic
+                BookIssuing::issueBook(library);
                 break;
 
             case 2:
-                library.push_back(new novel());
-                library.back()->getdata();
-                break;
+                while (true) {
+                    cout << "\nAdd a Book:" << endl;
+                    cout << "1. Add Textbook" << endl;
+                    cout << "2. Add Novel" << endl;
+                    cout << "3. Add Reference Book" << endl;
+                    cout << "4. Add Fiction Book" << endl;
+                    cout << "0. Back to Main Menu" << endl;
+                    cout << "Enter your choice: ";
+                    int addChoice;
+                    cin >> addChoice;
 
-            case 3:
-                library.push_back(new reference_book());
-                library.back()->getdata();
-                break;
+                    switch (addChoice) {
+                        case 1:
+                            library.push_back(new textbook());
+                            library.back()->getdata();
+                            break;
 
-            case 4:
-                library.push_back(new fiction());
-                library.back()->getdata();
-                break;
+                        case 2:
+                            library.push_back(new novel());
+                            library.back()->getdata();
+                            break;
 
-            case 5:
-                cout << "\nList of Books:" << endl;
-                for (const auto& item : library)
-                {
-                    item->putdata();
-                    cout << endl;
+                        case 3:
+                            library.push_back(new reference_book());
+                            library.back()->getdata();
+                            break;
+
+                        case 4:
+                            library.push_back(new fiction());
+                            library.back()->getdata();
+                            break;
+
+                        case 0:
+                            // Return to the main menu
+                            break;
+
+                        default:
+                            cout << "Invalid choice. Please try again." << endl;
+                    }
+
+                    // Ask if the user wants to add more books
+                    cout << "Do you want to add more books? (1 for yes, 0 for no): ";
+                    int addMore;
+                    cin >> addMore;
+
+                    if (addMore == 0) {
+                        break; // Exit the add books loop
+                    }
                 }
                 break;
 
             case 0:
-                // Clean up memory
+                // Clean up memory and exit
                 for (auto item : library)
                 {
                     delete item;
                 }
+                cout << "Thanks for using this system." << endl;
                 return 0;
 
             default:
                 cout << "Invalid choice. Please try again." << endl;
-                break;
         }
     }
 
